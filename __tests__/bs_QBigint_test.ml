b85651d16179986c1c3310a551a9ef1b7add5a40
James Haver <mchaver@gmail.com>
2018-06-19 16:57:31 +0800

    Add tests

--- __tests__/bs_QBigint_test.ml
+++ __tests__/bs_QBigint_test.ml
@@ -5,4 +5,13 @@ let () =
 
 describe "QBigint" (fun () ->
   test "zero equal to (Bigint.of_int 0)" (fun () ->
+    expect @@ Q.RationalBigint.(sub (make (Z.ZBigint.one) (Z.ZBigint.of_int 2)) (make (Z.ZBigint.of_int 1) (Z.ZBigint.of_int 4)) ) |> toEqual (Q.RationalBigint.(make (Z.ZBigint.of_int 1) (Z.ZBigint.of_int 4)) ));
+
+                        
+  test "zero equal to (Bigint.of_int 0)" (fun () ->
     expect @@ Q.RationalBigint.zero |> toEqual (Q.RationalBigint.zero)))
+
+
+(*
+
+ *)
